acdd983aacf01a71109d3b43096b1db2d8a41b9d
aramis.matos-nieves <aramis.matos-nieves@us.af.mil>
2025-08-06 12:51:03 +0000

    added evil mode and keybindings

--- nixos/home/programs/emacs/scripts/init.el
+++ nixos/home/programs/emacs/scripts/init.el
@@ -12,7 +12,6 @@
   (add-hook mode (lambda () (display-line-numbers-mode 0))))
 (setq visible-bell t)
 (set-face-attribute 'default nil :font "FiraCode Nerd Font Mono" :height 180)
-(load-theme 'wombat)
 
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
@@ -30,8 +29,6 @@
 
 (require 'use-package)
 (setq use-package-always-ensure t)
-
-(use-package evil)
 
 (use-package ivy
   :diminish
@@ -52,6 +49,8 @@
   (ivy-mode 1))
 
 (use-package counsel)
+
+(use-package all-the-icons)
 
 (use-package doom-modeline
   :ensure t
@@ -109,3 +108,58 @@
   ([remap describe-command] . helpful-command)
   ([remap describe-variable] . counsel-describe-variable)
   ([remap describe-key] . describe-key))
+
+(use-package general
+  :config
+  (general-evil-setup t)
+  (general-create-definer rune/leader-keys
+			  :keymaps '(normal insert visual emacs)
+			  :prefix "SPC"
+			  :global-prefix "C-SPC")
+  (rune/leader-keys
+    "t" '(:ignore t :which-key "toggles")
+    "tt" '(counsel-load-theme :which-key "choose-theme")))
+
+(use-package evil
+  :init
+  (setq evil-want-integration t)
+  (setq evil-want-keybinding nil)
+  (setq evil-want-C-u-scroll t)
+  (setq evil-want-C-i-jump nil)
+  :hook (evil-mode . rune/evil-hook)
+  :config
+  (evil-mode 1)
+  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
+  (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
+  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
+  (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
+  (evil-set-initial-state 'messages-buffer-mode 'normal)
+  (evil-set-initial-state 'dashboard-mode 'normal))
+
+(use-package evil-collection
+  :after evil
+  :config
+  (evil-collection-init))
+
+(defun rune/evil-hook ()
+  (dolist (mode '(custom-mode
+		  eshell-mode
+		  git-rebase-mode
+		  erc-mode
+		  circle-server-mode
+		  circle-chat-mode
+		  circle-query-mode
+		  sauron-mode
+		  term-mode))
+    (add-to-list 'evil-emacs-state-modes mode)))
+
+(use-package hydra)
+
+(defhydra hydra-text-scale (:timeout 4)
+	  "scale text"
+	  ("j" text-scale-increase "in")
+	  ("k" text-scale-decrease "out")
+	  ("f" nil finished :exit t))
+
+(rune/leader-keys
+ "ts" '(hydra-text-scale/body : which-key "scale-text"))
